c2c3d9fcf219adf6cca0996464586906d5d943ee
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-09-13 20:14:07 +0900

    chap02 순환 예시

---
+++ factorial_main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int factorial(int n);
+int factorial_iter(int n);
+
+int main(void)
+{
+	printf("factorial is %d\n", factorial(3));
+	printf("factorial is %d\n", factorial_iter(3));
+
+	return 0;
+}
