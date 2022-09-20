6b615e44114b3c7e393b90c0416cb5ff1fcff672
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-09-20 21:15:15 +0900

    chap01

--- chap01/cal_time.cpp
+++ chap01/cal_time.cpp
@@ -5,13 +5,13 @@
 int main(void) {
 	clock_t start, stop;
 	double duration;
-	start = clock(); // 측정 시작
+	start = clock();
 	for (int i = 0; i < 1000000; i++)
 		;
-	stop = clock(); // 측정 종료
+	stop = clock();
 
 	duration = (double)(stop - start) / CLOCKS_PER_SEC;
-	printf("수행시간은 %f초입니다.\n", duration);
+	printf("blank.\n", duration);
 
 	return 0;
 }
